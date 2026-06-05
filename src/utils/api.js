18baebd2d4d6662ad0779457fc83a2c535fe2315
kinyanjuibilly5-wq <kinyanjuibilly5@gmail.com>
2026-06-05 21:21:30 +0300

    fix api.js syntax error

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
- baseURL: 'https://horton-dental-clinic-portal.onrender.com/api',
+  baseURL: 'https://horton-dental-clinic-portal.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
