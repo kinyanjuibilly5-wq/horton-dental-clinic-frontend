7301d986eed9023a19179d653e46926b66dde7f3
kinyanjuibilly5-wq <kinyanjuibilly5@gmail.com>
2026-06-05 22:22:25 +0300

    fix api.js with correct backend URL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://horton-dental-clinic-portal.onrender.com/api',
+  baseURL: 'https://dental-clinic-backend-1.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
-  if (token) config.headers['x-auth-token'] = token;
+  if (token) {
+    config.headers['x-auth-token'] = token;
+  }
   return config;
 });
 
